e109732b69d088653ce706c94aca0d8ce7a3ac62
nurchyn <nurcahyono320@gmail.com>
2020-10-18 15:59:49 +0700

    sndkbas

--- application/views/v_content.php
+++ application/views/v_content.php
@@ -80,6 +80,65 @@
                 <!-- Divider Ends -->
                 <!-- About Content Starts -->
                 <div class="row about-content">
+                    <p style="text-align:justify">Di zaman social distancing ini, tidak hanya manusia saja yang berjauhan tetapi bisnis pun ikut berjauhan.
+                    Maka dari itu untuk bisa meledeknya bisnis tidak bisa hanya offline saja, namun harus beralih ke bisnis
+                    online. Agar Bisnis Online bisa maju dan Meledak, maka kita membutuhkan Ilmu Digital Marketing. Jika
+                    anda tidak memiliki Ilmu Digital Marketing, maka Bisnis Online tidak akan bisa Meledak. Maka itu butuh
+                    Ilmu Digital Marketing untuk Bisnis Online agar bisa berkembang maju.</p>
+                    <p style="text-align:justify">Jika saat ini tidak mengetahui Ilmu Digital Marketing, maka akan mengalami kusulitan untuk bersaing
+                    dan mengembangkan bisnis kita. Fakta-faktanya pun sudah sering kita lihat, yaitu :
+                    </p>
+                    <table width="100%" style="margin-left:20px!important;">
+                        <tbody>
+                            <tr>
+                                <td>1.</td>
+                                <td style="text-align:justify">Merek / bisnis yang sudah besar saja seperti pizza hut pun harus rela turun ke jalan, apalagi
+                                merek yang masih kecil dan belum dikenal oleh masyarakat.</td>
+                            </tr>
+                            <tr>
+                                <td>2.</td>
+                                <td style="text-align:justify">Maunya gratisan, hanya modal foto produk bagus dan main asal post-post di instagram dan
+                                facebook. Itu pun tidak disebarkan ke semua teman/followers kita.</td>
+                            </tr>
+                            <tr>
+                                <td>3.</td>
+                                <td style="text-align:justify">Broadcast promosi di Whatsapp, itu pun juga butuh kontak yang banyak dan juga bisa di blok
+                                karena spam.</td>
+                            </tr>
+                            <tr>
+                                <td>4.</td>
+                                <td style="text-align:justify">Mengadakan Giveaway terus menerus, agar banyak yg berkunjung. Iya kalau banyak yang beli,
+                                kalau hanya ingin Giveawaynya saja? Kita malah tekor sudah mengeluarkan hadiah banyak, tapi
+                                tidak ada yang beli.</td>
+                            </tr>
+                            <tr>
+                                <td>5.</td>
+                                <td style="text-align:justify">Sudah turun ke jalan buat jualan, upload-upload foto di semua sosial media, broadcastbroadcast ke banyak orang, sampai mengadakan Giveaway, tapi malah rugi bahkan tidak
+                                dapatkan penghasilan dan Bisnis Online tidak meledak, terancam tutup.</td>
+                            </tr>
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td style="text-align:justify">Ayo Pacaran dgn Saras Davina, kl tidak pacaran mana mungkin bisnis anda bisa Meledak
+                            </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <p style="text-align:justify">Pacaran ada 2 macam :
+                    </p>
+                    <table width="100%" style="margin-left:20px!important;">
+                        <tbody>
+                            <tr>
+                                <td>1.</td>
+                                <td style="text-align:justify">Pertemuan / kelas bersama</td>
+                            </tr>
+                            <tr>
+                                <td>2.</td>
+                                <td style="text-align:justify">Pertemuan / kelas personal</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <br>
+                    <br>
                     <div class="col-sm-12 col-md-12 col-lg-6 about-left-side">
                         <h3 class="title-about">WE ARE <strong>SALIMO</strong></h3>
                         <hr>
@@ -770,7 +829,7 @@
                     <div class="col-sm-12 col-md-4 col-xs-12">
                         <div class="latest-post">
                             <!-- Featured Image Starts -->
-                            <a class="img-thumb" href="blog-post.html"><img class="img-fluid" src="http://via.placeholder.com/720x477" alt="img"></a>
+                            <a class="img-thumb" href="blog-post.html"><img class="img-fluid" src="<?php echo base_url();?>assets/images/blog-post-small-1.jpg" alt="img"></a>
                             <!-- Featured Image Ends -->
                             <!-- Article Content Starts -->
                             <div class="post-body">
@@ -795,7 +854,7 @@
                     <div class="col-sm-12 col-md-4 col-xs-12">
                         <div class="latest-post">
                             <!-- Featured Image Starts -->
-                            <a class="img-thumb" href="blog-post.html"><img class="img-fluid" src="http://via.placeholder.com/720x477" alt="img"></a>
+                            <a class="img-thumb" href="blog-post.html"><img class="img-fluid" src="<?php echo base_url();?>assets/images/blog-post-small-2.jpg" alt="img"></a>
                             <!-- Featured Image Ends -->
                             <!-- Article Content Starts -->
                             <div class="post-body">
@@ -820,7 +879,7 @@
                     <div class="col-sm-12 col-md-4 col-xs-12">
                         <div class="latest-post">
                             <!-- Featured Image Starts -->
-                            <a class="img-thumb" href="blog-post.html"><img class="img-fluid" src="http://via.placeholder.com/720x477" alt="img"></a>
+                            <a class="img-thumb" href="blog-post.html"><img class="img-fluid" src="<?php echo base_url();?>assets/images/blog-post-small-3.jpg" alt="img"></a>
                             <!-- Featured Image Ends -->
                             <!-- Article Content Starts -->
                             <div class="post-body">
@@ -874,142 +933,4 @@
         </section>
         <!-- Video Section Ends -->
         <!-- Contact Section Starts -->
-        <section id="contact" class="contact">
-            <!-- Container Starts -->
-            <div class="container">
-                <!-- Main Heading Starts -->
-                <div class="text-center top-text">
-                    <h1><span>Contact</span> Us</h1>
-                    <h4>Get in Touch</h4>
-                </div>
-                <!-- Main Heading Starts -->
-                <!-- Divider Starts -->
-                <div class="divider text-center">
-                    <span class="outer-line"></span>
-                    <span class="fa fa-envelope" aria-hidden="true"></span>
-                    <span class="outer-line"></span>
-                </div>
-                <!-- Divider Ends -->
-            </div>
-            <!-- Container Ends -->
-            <!-- Map Section Starts -->
-            <div class="info-map">
-                <div class="google-map">
-                    <div class="gmap_container">
-                        <div id="gmap_canvas"></div>
-                    </div>
-                </div>
-            </div>
-            <!-- Info Map Boxes Starts -->
-            <div class="container">
-                <div class="row info-map-boxes">
-                    <!-- Left Info Map Box Starts -->
-                    <div class="col-md-6 col-sm-12">
-                        <div class="info-map-boxes-item fa fa-clock-o">
-                            <h1>Working Hours</h1>
-                            <p>Monday-Thursday : 08:00–17:00
-                                <br>Friday & Saturday : 08:00–12:30
-                                <br>Sunday - Close</p>
-                        </div>
-                    </div>
-                    <!-- Left Info Map Box Ends -->
-                    <!-- Right Info Map Box Starts -->
-                    <div class="col-md-6 col-sm-12">
-                        <div class="info-map-boxes-item fa fa-phone">
-                            <h1>Phone &amp; Online</h1>
-                            <p>Phone : +39 1873 2878
-                                <br>Fax : +49 (0) 123 456789-15
-                                <br><a href="#">salimo@yourdomain.com</a></p>
-                        </div>
-                    </div>
-                    <!-- Right Info Map Box Ends -->
-                </div>
-            </div>
-            <!-- Info Map Boxes Ends -->
-        </section>
-        <!-- Contact Section Ends -->
-        <!-- Contact Form Section Starts -->
-        <section class="contactform">
-            <div class="section-overlay">
-                <div class="container">
-                    <!-- Main Heading Starts -->
-                    <div class="text-center top-text">
-                        <h1><span>Send Us</span> an email</h1>
-                        <h4>We are very responsive to messages</h4>
-                    </div>
-                    <!-- Main Heading Ends -->
-                    <div class="form-container">
-                        <!-- Contact Form Starts -->
-                        <form class="formcontact" method="post" action="php/process-form.php">
-                            <div class="row form-inputs">
-                                <!-- First Name Field Starts -->
-                                <div class="col-md-6 form-group custom-form-group">
-                                    <span class="input custom-input">
-										<input placeholder="First Name" class="input-field custom-input-field" id="firstname" name="firstname" type="text" required data-error="NEW ERROR MESSAGE">
-										<label class="input-label custom-input-label" >
-											<i class="fa fa-user icon icon-field"></i>
-										</label>
-									</span>
-                                </div>
-                                <!-- First Name Field Ends -->
-                                <!-- Last Name Field Starts -->
-                                <div class="col-md-6 form-group custom-form-group">
-                                    <span class="input custom-input">
-										<input placeholder="Last Name" class="input-field custom-input-field" id="lastname" name="lastname" type="text" required>
-										<label class="input-label custom-input-label" >
-											<i class="fa fa-user-o icon icon-field"></i>
-										</label>
-									</span>
-                                </div>
-                                <!-- Last Name Field Ends -->
-                                <!-- Message Field Starts -->
-                                <div class="form-group custom-form-group col-md-12">
-                                    <textarea placeholder="Message" id="message" name="message" cols="45" rows="7" required></textarea>
-                                </div>
-                                <!-- Message Field Ends -->
-                                <!-- Email Name Field Starts -->
-                                <div class="col-md-6 form-group custom-form-group">
-                                    <span class="input custom-input">
-										<input placeholder="Email" class="input-field custom-input-field" id="email" name="email" type="text" required>
-										<label class="input-label custom-input-label" >
-											<i class="fa fa-envelope icon icon-field"></i>
-										</label>
-									</span>
-                                </div>
-                                <!-- Email Name Field Ends -->
-                                <!-- Submit Button Starts -->
-                                <div class="col-md-6 submit-form">
-                                    <button id="form-submit" name="submit" type="submit" class="custom-button" title="Send">Send Message</button>
-                                </div>
-                                <!-- Submit Button Ends -->
-                                <!-- Form Submit Message Starts -->
-                                <div class="col-sm-12 text-center output_message_holder d-none">
-                                    <p class="output_message"></p>
-                                </div>
-                                <!-- Form Submit Message Ends -->
-                            </div>
-                        </form>
-                        <!-- Contact Form Ends -->
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Contact Form Section Ends -->
-        <!-- Logos Section Starts -->
-        <section class="logos">
-            <div class="container">
-                <ul class="bxslider" id="bxslider">
-                    <!-- Logos Items Starts -->
-                    <li><img src="http://via.placeholder.com/150x29" alt=""></li>
-                    <li><img src="http://via.placeholder.com/150x29" alt=""></li>
-					<li><img src="http://via.placeholder.com/150x29" alt=""></li>
-					<li><img src="http://via.placeholder.com/150x29" alt=""></li>
-					<li><img src="http://via.placeholder.com/150x29" alt=""></li>
-					<li><img src="http://via.placeholder.com/150x29" alt=""></li>
-					<li><img src="http://via.placeholder.com/150x29" alt=""></li>
-					<li><img src="http://via.placeholder.com/150x29" alt=""></li>
-                    <!-- Logos Items Ends -->
-                </ul>
-            </div>
-        </section>
-        <!-- Logos Section Ends -->
+       
