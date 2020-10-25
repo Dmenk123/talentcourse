ff03c2e0744254ce56f63756b882f19d2dde2dc6
Dmenk123 <rizkiyuandaa@gmail.com>
2020-10-25 20:53:32 +0700

    aieoaoeiaeoiaeoia

---
+++ bo/application/modules/set_video_konten/controllers/Set_video_konten.php
@@ -0,0 +1,186 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Set_video_konten extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		if($this->session->userdata('logged_in') === false) {
+			return redirect('login');
+		}
+
+		$this->load->model('t_video_konten');
+		$this->load->model('m_global');
+		$this->load->model('master_user/m_user');
+	}
+
+	public function index()
+	{
+		$id_user = $this->session->userdata('id_user');
+		$data_user = $this->m_user->get_by_id($id_user);
+
+		$select = "tvk.*, mt.nama as nama_talent, tvt.path_video";
+		$where = ['tvk.id_talent' => 1, 'tvk.deleted_at' => null];
+		$join = [ 
+			['table' => 'm_talent mt', 'on' => 'tvk.id_talent = mt.id'],
+			['table' => 't_video_talent tvt', 'on' => 'tvk.id_t_video_talent = tvt.id'],
+		];
+		$data_galeri = $this->m_global->multi_row($select, $where, 't_video_konten as tvk', $join, 'tvk.urutan');
+		
+		/**
+		 * data passing ke halaman view content
+		 */
+		$data = array(
+			'title' => 'Setting Video Konten',
+			'data_user' => $data_user,
+			'data_galeri' => $data_galeri
+		);
+
+		/**
+		 * content data untuk template
+		 * param (css : link css pada direktori assets/css_module)
+		 * param (modal : modal komponen pada modules/nama_modul/views/nama_modal)
+		 * param (js : link js pada direktori assets/js_module)
+		 */
+		$content = [
+			'css' 	=> null,
+			'modal' => null,
+			'js'	=> 'set_video_konten.js',
+			'view'	=> 'view_set_video_konten'
+		];
+
+		$this->template_view->load_view($content, $data);
+	}
+
+ 	/**
+	 * get data untuk select2 (dipakai dimana-mana)
+	 */
+	public function select_video_konten()
+	{
+		$term = $this->input->get('term');
+		$select = "m_talent.id as id_talent, m_talent.nama, t_video_talent.id, t_video_talent.path_video";
+		$join = [ 
+			['table' => 't_video_talent', 'on' => 'm_talent.id = t_video_talent.id_talent']
+		];
+		$data_galeri = $this->m_global->multi_row($select, ['m_talent.id' => 1, 't_video_talent.deleted_at' => null, 't_video_talent.path_video like' => '%'.$term.'%'], 'm_talent', $join, 't_video_talent.id');
+		
+		// echo "<pre>";
+		// print_r ($data_galeri);
+		// echo "</pre>";
+		// exit;
+
+		if($data_galeri) {
+			foreach ($data_galeri as $key => $value) {
+				$row['id'] = $value->id;
+				$row['text'] = $value->nama.' - '.$value->path_video;
+				$row['path'] = $value->path_video;
+				$retval[] = $row;
+			}
+		}else{
+			$retval = false;
+		}
+		echo json_encode($retval);
+	}
+
+	public function add_data()
+	{
+		$obj_date = new DateTime();
+		$timestamp = $obj_date->format('Y-m-d H:i:s');
+		$arr_valid = $this->rule_validasi();
+		
+		$urutan = $this->input->post('urutan');
+		$id_t_video_talent = $this->input->post('sel_video');
+		$id_talent = 1; //cuma 1 aja
+		
+		if ($arr_valid['status'] == FALSE) {
+			echo json_encode($arr_valid);
+			return;
+		}
+
+		$this->db->trans_begin();
+
+		//ambil data existing
+		$cek = $this->t_video_konten->get_by_condition("id_talent = '$id_talent' and urutan = '$urutan' and deleted_at is null", true);
+		if($cek) {	
+			$data['inputerror'][] = 'urutan';
+            $data['error_string'][] = 'Urutan Sudah ada';
+            $data['status'] = FALSE;
+			echo json_encode($data);
+			return;
+		}
+
+		$id = $this->t_video_konten->get_max_id();
+		
+		$datanya['id'] = $id;
+		$datanya['id_talent'] = $id_talent;
+		$datanya['id_t_video_talent'] = $id_t_video_talent;
+		$datanya['urutan'] = $urutan;
+		$datanya['created_at'] = $timestamp;
+		
+		$insert = $this->t_video_konten->save($datanya);
+		
+		if ($this->db->trans_status() === FALSE){
+			$this->db->trans_rollback();
+			$retval['status'] = false;
+			$retval['pesan'] = 'Gagal setting Video Konten';
+		}else{
+			$this->db->trans_commit();
+			$retval['status'] = true;
+			$retval['pesan'] = 'Sukses setting Video Konten';
+		}
+
+		echo json_encode($retval);
+	}
+
+	public function delete_data()
+	{
+		$id = $this->input->post('id');
+		$del = $this->t_galeri_konten->softdelete_by_id($id);
+		
+		if($del) {
+			$retval['status'] = TRUE;
+			$retval['pesan'] = 'Data Galeri Sukses dihapus';
+		}else{
+			$retval['status'] = FALSE;
+			$retval['pesan'] = 'Data Galeri Gagal dihapus';
+		}
+
+		echo json_encode($retval);
+	}
+
+	private function rule_validasi()
+	{
+		$data = array();
+		$data['error_string'] = array();
+		$data['inputerror'] = array();
+		$data['status'] = TRUE;
+
+		if ($this->input->post('urutan') == '') {
+			$data['inputerror'][] = 'urutan';
+            $data['error_string'][] = 'Wajib Memilih Nama Urutan';
+            $data['status'] = FALSE;
+		}
+
+		if ($this->input->post('sel_video') == '') {
+			$data['inputerror'][] = 'sel_video';
+            $data['error_string'][] = 'Wajib Memilih Nama Video';
+            $data['status'] = FALSE;
+		}
+        return $data;
+	}
+
+	///////////////////////////////////////////////////////
+
+	private function seoUrl($string) {
+	    //Lower case everything
+	    $string = strtolower($string);
+	    //Make alphanumeric (removes all other characters)
+	    $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+	    //Clean up multiple dashes or whitespaces
+	    $string = preg_replace("/[\s-]+/", " ", $string);
+	    //Convert whitespaces and underscore to dash
+	    $string = preg_replace("/[\s_]/", "-", $string);
+	    return $string;
+	}
+}
---
+++ bo/assets/js_module/set_video_konten.js
@@ -0,0 +1,155 @@
+var save_method;
+var table;
+
+$(document).ready(function() {
+
+    //force integer input in textfield
+    $('input.numberinput').bind('keypress', function (e) {
+        return (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57) && e.which != 46) ? false : true;
+    });
+    
+    $("#sel_video").select2({
+        // tags: true,
+        //multiple: false,
+        tokenSeparators: [',', ' '],
+        minimumInputLength: 0,
+        minimumResultsForSearch: 5,
+        ajax: {
+            url: base_url+'set_video_konten/select_video_konten',
+            dataType: "json",
+            type: "GET",
+            data: function (params) {
+
+                var queryParameters = {
+                    term: params.term
+                }
+                return queryParameters;
+            },
+            processResults: function (data) {
+                return {
+                    results: $.map(data, function (item) {
+                        return {
+                            text: item.text,
+                            id: item.id,
+                            path: item.path,
+                        }
+                    })
+                };
+            }
+        }
+    });
+
+    $(".modal").on("hidden.bs.modal", function(){
+        reset_modal_form();
+    });
+});	
+
+function delete_galeri(id){
+    swalConfirmDelete.fire({
+        title: 'Hapus Galeri ?',
+        text: "Data Galeri Akan Di Stop ?",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Ya, Hapus Galeri !',
+        cancelButtonText: 'Tidak, Batalkan!',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.value) {
+            $.ajax({
+                url : base_url + 'set_galeri_konten/delete_data',
+                type: "POST",
+                dataType: "JSON",
+                data : {id:id},
+                success: function(data)
+                {
+                    swalConfirm.fire('Berhasil Hapus Diskon!', data.pesan, 'success');
+                    location.reload();
+                },
+                error: function (jqXHR, textStatus, errorThrown)
+                {
+                    Swal.fire('Terjadi Kesalahan');
+                }
+            });
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalConfirm.fire(
+            'Dibatalkan',
+            'Aksi Dibatalakan',
+            'error'
+          )
+        }
+    });
+}
+
+function save()
+{
+    var form = $('#form_galeri')[0];
+    var data = new FormData(form);
+    
+    $("#btnSave").prop("disabled", true);
+    $('#btnSave').text('Menyimpan Data'); //change button text
+    $.ajax({
+        type: "POST",
+        enctype: 'multipart/form-data',
+        url: base_url + 'set_video_konten/add_data',
+        data: data,
+        dataType: "JSON",
+        processData: false,
+        contentType: false, 
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+            if(data.status) {
+                swal.fire("Sukses!!", "Berhasil Menambahkan Video", "success");
+                location.reload();
+            }else {
+                for (var i = 0; i < data.inputerror.length; i++) 
+                {
+                    if (data.inputerror[i] != 'sel_video') {
+                        $('[name="'+data.inputerror[i]+'"]').addClass('is-invalid');
+                        $('[name="'+data.inputerror[i]+'"]').next().text(data.error_string[i]).addClass('invalid-feedback');
+                    }else{
+                        //ikut style global
+                        $('[name="'+data.inputerror[i]+'"]').next().next().text(data.error_string[i]).addClass('invalid-feedback-select');
+                    }
+                }
+
+                $("#btnSave").prop("disabled", false);
+                $('#btnSave').text('Simpan');
+            }
+        },
+        error: function (e) {
+            console.log("ERROR : ", e);
+            $("#btnSave").prop("disabled", false);
+            $('#btnSave').text('Simpan');
+
+            reset_modal_form();
+            $(".modal").modal('hide');
+        }
+    });
+}
+
+function reset_modal_form()
+{
+    $('#form-talent-aktif')[0].reset();
+    $('.append-opt').remove(); 
+    $('div.form-group').children().removeClass("is-invalid invalid-feedback");
+    $('span.help-block').text('');
+    $('.kt-select2').val('').trigger('change');
+}
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        $('#div_preview_foto').css("display","block");
+        $('#preview_img').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    } else {
+        $('#div_preview_foto').css("display","none");
+        $('#preview_img').attr('src', '');
+    }
+}
